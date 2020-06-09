40688bfa49f1fb5718bc5ef26912412e18a5e1c9
Toytum <steath1999@gmail.com>
2020-06-09 10:13:17 -0700

    Trying to get fork to work for pull request

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,20 @@
 require_relative 'board'
 
-class Fixme
+class Game
+  be_over = false
+  @board = Board.new()
+  def init(game_board = nil)
+    @board = game_board
+  end
+  def board
+    board
+  end
+  def end
+    if board.full?
+      be_over = true
+    end
+    return be_over
+  end
+
+
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,9 +1,46 @@
 require_relative '../lib/board'
 
 describe 'A Tic Tac Toe board' do
-
-  it 'needs covered with tests' do
-    fail 'See spec/board_spec.rb'
+  
+  it 'exists' do
+    Board.new
   end
 
+  it 'can place tokens at locations' do
+    game_board = Board.new
+    game_board.place("FakeToken", :top , :left)
+    expect(game_board.token_at(:top, :left)).to eq("FakeToken")
+  end
+
+  it 'can reset all loactions' do 
+    game_board = Board.new
+    game_board.place("FakeToken2", :bottom , :right)
+    game_board.reset
+    expect(game_board.token_at(:bottom, :right)).to eq(nil)
+  end
+
+  it 'can check to see if board is empty' do
+    game_board = Board.new
+    game_board.place("FakeToken3", :middle, :middle)
+    expect(game_board.empty?).to eq(false)
+  end
+
+  it 'can check to see if board is full' do
+    game_board = Board.new
+    game_board.place("FakeToken4", :top, :left)
+    game_board.place("FakeToken5", :top, :middle)
+    game_board.place("FakeToken6", :top, :right)
+    game_board.place("FakeToken7", :middle, :left)
+    game_board.place("FakeToken8", :middle, :middle)
+    game_board.place("FakeToken9", :middle, :right)
+    game_board.place("FakeToken10", :bottom, :left)
+    game_board.place("FakeToken11", :bottom, :middle)
+    game_board.place("FakeToken12", :bottom, :right)
+    expect(game_board.full?).to eq(true)
+  end
+
+  # it 'can display board' do
+  #   game_board = Board.new
+  #   game_board.place("FakeToken13", :middle, :middle)
+  # end
 end
